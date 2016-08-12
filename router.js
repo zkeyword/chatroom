19c65f33ea18bf2c815f9b14287a55e2eff44fc7
jianxin.zhu <zjx1247225@163.com>
2016-08-12 14:33:00 +0800

    添加登录功能

--- router.js
+++ router.js
@@ -1,9 +1,35 @@
 let express  = require('express'),
 	router   = express.Router();
+	
+router.get('/', function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+	res.write("还没登录");
+	res.end();
+});
 
 router.get('/im', function(req, res) {
-	global.user = req.query.user;
-    res.render('index', { username: req.query.user });
+	if( !req.session.user ) return res.redirect('/');
+	global.userInfo = req.query;
+	global.userInfo.user = req.session.user;
+    res.render('index', { username: req.session.user });
+});
+
+router.get('/login', function(req, res) {
+	res.render('login');
+});
+
+router.post('/login', function(req, res) {
+	req.session.user = req.body.username;
+	console.log( req.body )
+	res.write("登录成功");
+	res.end();
+});
+
+
+router.get('/Logout', function(req, res) {
+	req.session.destroy();
+	res.write("登出成功");
+	res.end();
 });
 	
 module.exports = router;
